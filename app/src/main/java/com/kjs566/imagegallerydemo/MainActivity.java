6a731e11b64153b48181cdb806e2ab4b8a545922
Jan Skála <jan.skala566@gmail.com>
2018-06-17 19:24:07 +0200

    created adapters, added samples

--- app/src/main/java/com/kjs566/imagegallerydemo/MainActivity.java
+++ app/src/main/java/com/kjs566/imagegallerydemo/MainActivity.java
@@ -1,13 +1,35 @@
 package com.kjs566.imagegallerydemo;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.kjs566.imagegallery.ui.IGDetailActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final @IdRes int[] buttons = {R.id.btn_from_resources, R.id.btn_from_uris};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for(int id : buttons){
+            findViewById(id).setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_from_resources:
+                IGDetailActivity.startActivity(this, R.drawable.borek00, R.drawable.borek01, R.drawable.borek02, R.drawable.borek03);
+                break;
+            case R.id.btn_from_uris:
+                IGDetailActivity.startActivity(this, getResources().getStringArray(R.array.image_urls));
+                break;
+        }
     }
 }
